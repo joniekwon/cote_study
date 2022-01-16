d8debd9e650917ac8dc09cbb409977403e6db3f9
joniekwon <93701351+joniekwon@users.noreply.github.com>
2022-01-16 23:51:35 +0900

    Create 크레인 인형뽑기 게임.py

---
+++ 프로그래머스_Level1/크레인 인형뽑기 게임.py
@@ -0,0 +1,12 @@
+def solution(board, moves):
+    answer = 0
+
+    res= []
+
+
+    return answer
+
+if __name__ == "__main__":
+    board = [[0, 0, 0, 0, 0], [0, 0, 1, 0, 3], [0, 2, 5, 0, 1], [4, 2, 4, 4, 2], [3, 5, 1, 3, 1]]
+    moves = [1,5,3,5,1,2,1,4]
+    solution(board,moves)
