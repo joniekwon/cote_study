f433a3a4ee25ee00cf5f169d77ce5d7fc9d7e789
joniekwon <93701351+joniekwon@users.noreply.github.com>
2022-03-18 19:04:28 +0900

    Update 미로 탈출.py

--- 이것이 코딩 테스트다/DFS,BFS/미로 탈출.py
+++ 이것이 코딩 테스트다/DFS,BFS/미로 탈출.py
@@ -3,25 +3,28 @@
 from collections import deque
 
 def solution(N,M, maps):
-    answer = 0
     visit = deque()
-    visit.append([0,0])
-    x, y = 0, 0
-    while True:
-        if (x+1,y+1) == (N, M):
-            break
-        x, y = visit.popleft()
-        answer+=1
-        if y+1<M and maps[x][y+1] !=0:
-            maps[x][y+1] = answer
-            visit.append([x,y+1])
-        if x+1<N and maps[x+1][y] !=0:
-            maps[x + 1][y] = answer
-            visit.append([x+1,y])
-    return answer
+    visit.append([0,0]) # 현재 위치
 
-# 1. 4가지 방향이 아닌 오른쪽, 아래쪽만으로 이동 --> 4방향으로 수정 필요,
-# 2. 처음 방문하는게 아닐때는 거리 기록하지 않도록 해야함.
+    # 이동할 방향 정의
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, 1, -1]
+
+    while visit:
+        x, y = visit.popleft()
+        for i in range(4):
+            nx, ny = x+dx[i], y+dy[i]
+            # 맵을 벗어날 수 없음
+            if nx<0 or ny<0 or nx>=N or ny>=M:
+                continue
+            # 괴물이 있는곳은 가지 않음
+            if maps[nx][ny]==0:
+                continue
+            # 처음 방문할 경우에만 최단거리 기록
+            if maps[nx][ny]==1:
+                maps[nx][ny] = maps[x][y]+1
+                visit.append([nx,ny])
+    return maps[N-1][M-1]
 
 if __name__=='__main__':
     N, M = 5, 6
