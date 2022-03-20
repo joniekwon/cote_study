fb35a7a91d7d6a18e7efba52eb4a848670a59c73
joniekwon <93701351+joniekwon@users.noreply.github.com>
2022-03-20 11:10:27 +0900

    Update 무지의 먹방 라이브.py

--- 이것이 코딩 테스트다/그리디/무지의 먹방 라이브.py
+++ 이것이 코딩 테스트다/그리디/무지의 먹방 라이브.py
@@ -1,3 +1,7 @@
+# 각 음식을 다 먹는데 걸리는 시간(초)가 food_times로 주어짐
+# 1초마다 음식을 바꾸어 먹고, 다 먹은 음식은 건너 뛴다.
+# 1번 음식부터 먹기 시작한 후 k초 뒤에 먹을 음식의 번호를 구해보자. 먹을 음식이 없으면 -1을 반환
+# 아직 미구현
 import heapq
 
 def solution(food_times, k):
@@ -6,9 +10,22 @@ def solution(food_times, k):
         return -1
 
     q = []
+    # 모든 음식을 우선순위 큐에 넣음
     for i in range(len(food_times)):
         heapq.heappush(q, (food_times[i], i + 1))
-    time = 0; l = len(food_times)
+
+    # 총 소요시간
+    total = 0
+    # 남은 음식 개수
+    l = len(food_times)
+    # 이전에 먹은 음식 시간
+    previous = 0
+
+    # 총 소요시간이 k보다 적고, 음식개수가 하나 이상일 때
+    while total<k and l>0:
+        # 가장 적게 소요되는 음식부터 큐에서 빼고 걸린 시간을 체크
+        now = heapq.heappop(q)[0]
+
 
     
 
