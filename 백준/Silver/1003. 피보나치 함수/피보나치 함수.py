e72e594c78bb6b032629b105d6997a6dc5ac0da3
joniekwon <93701351+joniekwon@users.noreply.github.com>
2022-05-19 18:15:36 +0900

    [Silver III] Title: 피보나치 함수, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    if n == 0:
+        print(1, 0)
+        continue
+    dp = [[]] * (n + 1)
+    dp[0] = [1, 0]
+    dp[1] = [0, 1]
+    for i in range(2, n + 1):
+        dp[i] = [dp[i - 2][0] + dp[i - 1][0], dp[i - 2][1] + dp[i - 1][1]]
+    print(*dp[-1])
