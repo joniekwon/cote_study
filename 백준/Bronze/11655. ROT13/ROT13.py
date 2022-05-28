347d71d8e2bd109a6858e2e75bbaff327361a4e5
joniekwon <93701351+joniekwon@users.noreply.github.com>
2022-05-28 12:51:34 +0900

    [Bronze I] Title: ROT13, Time: 136 ms, Memory: 33548 KB -BaekjoonHub

--- 백준/Bronze/11655. ROT13/ROT13.py
+++ 백준/Bronze/11655. ROT13/ROT13.py
@@ -1,14 +1,14 @@
 import sys
 import string
 input = sys.stdin.readline
-upper = string.ascii_uppercase
-lower = string.ascii_lowercase
+upper = {i:x for i, x in zip(range(26), string.ascii_uppercase)}
+lower = {i:x for i, x in zip(range(26), string.ascii_lowercase)}
 strings = input().rstrip()
 answer = ''
 for s in strings:
     if s.isdigit() or s == ' ':
         answer += s
-    elif s in set(upper):
+    elif s.isupper():
         answer += upper[(ord(s) - 65 + 13) % 26]
     else:
         answer += lower[(ord(s) - 97 + 13) % 26]
